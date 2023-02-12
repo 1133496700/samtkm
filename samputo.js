ff58e220624c9524bd6c5fb30d65aa041894ab4a
tomiakd <tomih_akd@hotmail.com>
2023-02-12 15:29:47 -0300

    bueno esto pa ver q onda

---
+++ samputo.js
@@ -0,0 +1,26 @@
+
+let primerrespuesta = parseInt(prompt("Indica el numero correcto!! 1.) si queres que te coja un negro 2.) si queres que te coja mariano y ventu"))
+
+
+
+
+
+if(primerrespuesta === 1){
+    let primerrespuestaa = parseInt(prompt("entonces te coje un negro 1.) con forro 2.) sin forro"))
+    if(primerrespuestaa === 1){
+        alert("me parece perfecto que te cuides")
+    } else if(primerrespuestaa === 2){
+        alert("aa mira que loca puta de mierda que sos e ")
+    }
+}
+
+if(primerrespuesta === 2){
+    let segundarespuesta = parseInt(prompt("entonces te cojen los pibes alto trolo. 1.) te acaban en la cara 2.) te la tomas toda y dsp viene el caniche de al lado y te coje tambien"))
+    if(segundarespuesta === 1){
+        alert("como te gusta la leche en la cara ee ")
+    } else if(segundarespuesta === 2){
+        alert("ese caniche puto de mierda ojala se coma un loro")
+    }
+} 
+
+
